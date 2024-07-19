2b60c99fc31eba8e840e351dbe26a22c8b86babd
BastianYakha <bastian@yakha.eu>
2024-07-19 18:15:52 +0200

    added first API test call

---
+++ KitchenAPI/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KitchenAPI.Controllers
+{
+    [Route("API/")]
+    public class ProductController : Controller
+    {
+        ProductHandler newHandler = new ProductHandler();
+
+        [HttpGet("GetTest")]
+        public async Task<JsonResult> testProduct()
+        {
+            var testProduct = newHandler.testProduct();
+            return Json(testProduct);
+        }
+    }
+}
---
+++ KitchenAPI/ProductHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace KitchenAPI
+{
+    public class ProductHandler
+    {
+        public KitchenIO.Objects.Product testProduct()
+        {
+            KitchenIO.Objects.Product product = new KitchenIO.Objects.Product();
+            product.Id = Guid.NewGuid();
+            product.name = "test";
+            product.barcode = Convert.ToInt32(product.name);
+            product.amount = 5;
+            product.price = 12.50;
+            product.weight = 120;
+
+            return product;
+        }
+    }
+}
