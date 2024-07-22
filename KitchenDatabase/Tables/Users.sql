b727bae619eca886eea7110a5e1722e2b41f56af
BastianYakha <bastian@yakha.eu>
2024-07-22 23:37:23 +0200

    Added Salt database Added Hash database Added Users database Added Account Screen Added different Allergy categories

---
+++ KitchenDatabase/Tables/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Password] VARCHAR(50) NOT NULL, 
+    [Allergies] VARCHAR(MAX) NULL
+)
