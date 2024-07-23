fb9aa5084297d4785ad9290b917c79832e9f2edb
BastianYakha <bastian@yakha.eu>
2024-07-23 16:42:00 +0200

    Recipe Search API Call added

---
+++ KitchenAPI/Controllers/RecipeController.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary.Objects;
+using KitchenAPI.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KitchenAPI.Controllers
+{
+    [Route("API/")]
+    public class RecipeController : Controller
+    {
+        RecipeHandler recipeHandler = new RecipeHandler();
+
+        [HttpGet("GetRecipes/{query}")]
+        public async Task<JsonResult> GetRecipes(string query)
+        {
+            List<Recipe> answer = await recipeHandler.GetAllRecipes(query);
+            return Json(answer);
+        }
+    }
+}
---
+++ KitchenAPI/Handlers/RecipeHandler.cs
@@ -0,0 +1,80 @@
+﻿using ClassLibrary.Objects;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace KitchenAPI.Handlers
+{
+    public class RecipeHandler
+    {
+        public async Task<List<Recipe>> GetAllRecipes(string query)
+        {
+
+            string api_key = "GDhZ4/8euUTcTtCOKOjlKA==UkjauVzTI4jMJJ8H";
+
+            List<Recipe> TotalList = new List<Recipe>();
+
+
+            using (HttpClient client = new HttpClient())
+            {
+                var offset = 0;
+                int totalList = 0;
+                int maxList = 100;
+
+
+                client.DefaultRequestHeaders.Add("X-Api-Key", api_key);
+
+                while(totalList < maxList)
+                {
+                    string url = $"https://api.api-ninjas.com/v1/recipe?query={Uri.EscapeDataString(query)}&offset={offset}";
+                    try
+                    {
+
+                        HttpResponseMessage response = await client.GetAsync(url);
+                        response.EnsureSuccessStatusCode();
+
+
+                        string responseBody = await response.Content.ReadAsStringAsync();
+
+
+                        List<Recipe> answer = JsonConvert.DeserializeObject<List<Recipe>>(responseBody);
+
+                        if (answer.Count > 0)
+                        {
+                            foreach (Recipe answerItem in answer)
+                            {
+                                TotalList.Add(answerItem);
+                            }
+                            offset = offset + 10;
+                        }
+                        else if (answer.Count == 0)
+                        {
+                            break;
+                        }
+
+
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        Console.WriteLine("\nException Caught!");
+                        Console.WriteLine("Message :{0} ", e.Message);
+                        return new List<Recipe>();
+                    }
+                    catch (JsonException e)
+                    {
+                        Console.WriteLine("\nJson Exception Caught!");
+                        Console.WriteLine("Message :{0} ", e.Message);
+                        return new List<Recipe>();
+                    }
+
+                    if(offset == maxList)
+                    {
+                        break;
+                    }
+                }
+                return TotalList;
+            }
+        }
+    }
+}
